50fabcceeb709193331f80b2c126f03954acc749
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2025-09-27 16:19:39 +0500

    feat: Commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+const Configurations = {
+    extends: ['@commitlint/cli', '@commitlint/config-conventional'],
+    rules:{
+        "type-enum": [2, "always", [
+            "build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"
+        ]],
+        "subject-case": [2, "always", "sentence-case"]
+    }
+}
+
+export default Configurations;
