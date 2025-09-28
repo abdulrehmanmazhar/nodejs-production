326d1d2f11e029965aa716e4550f1f3dc06600b2
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2025-09-28 12:36:27 +0500

    fix: Unused variables rule with _ convention in typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,23 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default defineConfig(
-  {
-    languageOptions:{
-        parserOptions:{
+export default defineConfig({
+    languageOptions: {
+        parserOptions: {
             project: true,
             tsconfigRootDir: import.meta.dirname
         }
     },
-    files: ["**/*.ts"],
-    ignores: ["node_modules/**", "dist/**"],
-    extends: [
-        eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  eslintConfigPrettier
-    ],
+    files: ['**/*.ts'],
+    ignores: ['node_modules/**', 'dist/**'],
+    extends: [eslint.configs.recommended, tseslint.configs.recommendedTypeChecked, eslintConfigPrettier],
     rules: {
-        "no-console": "error",
-        quotes: ["error", "single", {allowTemplateLiterals: true}],
+        'no-console': 'error',
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
     }
-  }
-);
+})
